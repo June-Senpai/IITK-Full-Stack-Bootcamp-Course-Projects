f74b70d271eb7853cae50e98bd598d42cc3e895e
June-Senpai <keshav01juneja@gmail.com>
2024-06-12 19:03:48 +0530

    weather:search params

--- classOneProjects/src/components/Weather.tsx
+++ classOneProjects/src/components/Weather.tsx
@@ -1,35 +1,36 @@
-import { useState } from "react"
 import { useDebounce } from "../hooks/Debounce"
 import { useFetch } from "../lib/fetch"
 import { kelvinToCelsius } from "../util/kelvinToCelsius "
+import { useSearchParams } from "react-router-dom"
 
 const Weather = () => {
-  const [location, setLocation] = useState("delhi")
-  const debounce = useDebounce(location)
+  const [searchParams, setSearchParams] = useSearchParams({ q: "delhi" })
+  const locationQuery = searchParams.get("q") ?? "delhi"
+  const debouncedLocation = useDebounce(locationQuery)
 
   const { data, error, isLoading } = useFetch(
     "weather",
     `https://api.openweathermap.org/data/2.5/forecast`,
     {
-      q: debounce,
+      q: debouncedLocation,
       appid: import.meta.env.VITE_WEATHER_API,
     }
   )
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocation(e.target.value)
+    setSearchParams({ q: e.target.value })
   }
 
   return (
     <div>
       <h1>Weather</h1>
-      <input type="text" value={location} onChange={handleChange} />
+      <input type="text" value={locationQuery} onChange={handleChange} />
       {isLoading && <p>Loading...</p>}
       {error && <p>An error has occurred: {error.message}</p>}
       {data && !isLoading && !error && (
         <div>
           <h2>Current Conditions</h2>
-          <p>Temperature: {Math.round(kelvinToCelsius(data.list[0].main.temp))} °C</p>
+          <p>Temperature: {kelvinToCelsius(data.list[0].main.temp)} °C</p>
           <p>Temperature: {data.list[0].main.temp} °K</p>
         </div>
       )}
